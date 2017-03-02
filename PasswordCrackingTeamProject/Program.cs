46a77e55c04c396c0e5eae617ff444b1127f3125
CH <jeghedderchristoffer@gmail.com>
2017-03-02 10:26:42 +0100

    CS branch?

--- PasswordCrackingTeamProject/Program.cs
+++ PasswordCrackingTeamProject/Program.cs
@@ -11,6 +11,7 @@ namespace PasswordCrackingTeamProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine();
         }
     }
 }
